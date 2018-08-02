0c950fcff3702bbfc73f79df9f14d60669333499
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-08-02 21:32:12 +0200

    correction of algorithm mistakes, introduced some print for debugging, seems it works for a single trace. algorithm tested with all traces present in paper

--- janus.py
+++ janus.py
@@ -2,7 +2,7 @@ from janus.io.InputHandler import InputHandler
 from janus.formulas.separatedFormula import SeparatedFormula
 from janus.formulas.formula import Formula
 from janus.automata.sepautset import SeparatedAutomataSet
-import argparse
+import argparse, copy
 
 args_parser = argparse.ArgumentParser(description='Janus algorithm computes the interestingness degree of an RCon over'+
                                                   ' a trace taken by an event log L')
@@ -14,7 +14,7 @@ params = vars(args_parser.parse_args())
 
 input_log = InputHandler(params['<event-log>'])
 trace = input_log.event_log
-print('[LOG]: ' + str(trace))
+print('[TRACE]: ' + str(trace))
 activator = params['<activator>']
 print('[ACTIVATOR]: ' + activator)
 
@@ -24,30 +24,40 @@ sepFormula1 = SeparatedFormula(('Yb', 'T', 'T'))
 sepFormula2 = SeparatedFormula(('T', 'T', 'Ec'))
 
 constraint = Formula([sepFormula1, sepFormula2]) # set manually the constraint
-print('[CONSTRAINT]: ' + str(constraint))
+print('[SEPARATED FORMULAS]: ' + str(constraint))
 
 sepautset = SeparatedAutomataSet(constraint).automa_set
 
 # JANUS ALGORITHM SKETCH
 O = []
 for event in trace:
-    for past, now, future in sepautset:
-        past.make_transition(event)
+    for pastAut in sepautset:
+        pastAut[0].make_transition(event)
+        # if pastAut[0].symbol == 'b':
+        #     print('[PAST AUT ' + str(pastAut[0].symbol) + ' ]: ' + str(pastAut[0].current_state))
     if event == activator:
-        J = set()
-        # for past, now, future in sepautset:
-        if past.is_accepting() and now.accepts(event):
-            J.add((future.current_state, future))
+        #J = set()
+        J = {}
+        for past, now, future in sepautset:
+            if past.is_accepting() and now.accepts(event):
+                #print('[PAST ' + str(past.symbol) + ' IN ACCEPTING]: state == ' + str(past.current_state))
+                #J.add((future.initial_state, future))
+                temp = copy.deepcopy(future)
+                J[temp] = future.initial_state
+        #print('Janus is: ' + str(J))
         O.append(J)
+        #print('O bag is: ' + str(O))
+    #print('[OBAG EVENT ' + event + '] ' + str(O))
     for j in O:
-        for st, aut in J:
+        for aut, st in j.items():
             aut.make_transition(event)
-            st = aut.current_state
+            #print('aut {0}, state after transition {1}: {2}'.format(aut.symbol, event, aut.current_state))
+            j[aut] = aut.current_state
 
 if O:
     count = 0
     for janus in O:
-        for state, automa in janus:
+        for automa, state in janus.items():
             if state in automa.accepting_states:
                 count += 1
             else:
