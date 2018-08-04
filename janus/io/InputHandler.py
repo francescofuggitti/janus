f12c3d5a1839fc264a116a1e4326f79a39fd2a43
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-08-04 14:58:12 +0200

    return event_log as a set of traces made by events classified by classifiers

--- janus/io/InputHandler.py
+++ janus/io/InputHandler.py
@@ -6,25 +6,28 @@ class InputHandler:
 
     def __init__(self, input_path):
         self.input_path = input_path
-        self.event_log = self.load()
+        self._event_log = None
+        self.load()
+
+    @property
+    def event_log(self):
+        return self._event_log
 
     def load_txt(self):
         try:
             with open(self.input_path, 'r') as f:
-                self.event_log = set(tuple(i) for i in [f.read().splitlines()])
+                self._event_log = set(tuple(i) for i in [f.read().splitlines()])
                 f.close()
-            return self.event_log
         except:
             raise IOError('[ERROR]: Unable to import text file')
 
     def load_csv(self):
-        self.event_log = []
+        self._event_log = []
         try:
             with open(self.input_path, newline='', encoding='utf-8-sig') as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    self.event_log.append(row[0])
-            return self.event_log
+                    self._event_log.append(row[0])
         except:
             raise IOError('[ERROR]: Unable to import csv file')
 
@@ -35,13 +38,13 @@ class InputHandler:
 
             # get classifiers
             classifiers = []
-            for cl in log.get_classifier():
-                classifiers.append(cl)
+            for cl in log.get_classifiers():
+                classifiers.append(str(cl))
 
             classifier = XEventAttributeClassifier("activity", [classifiers[0]])
             log_list = list(map(lambda trace: list(map(classifier.get_class_identity, trace)), log))
 
-            self.event_log = set(tuple(trace) for trace in log_list)
+            self._event_log = set(tuple(trace) for trace in log_list)
 
             #
             # self.event_log = {}
@@ -55,17 +58,16 @@ class InputHandler:
             #             self.event_log[event_str] = 1
             #     event_str = ''
 
-            return self.event_log
         except:
             raise IOError('[ERROR]: Unable to import xes file')
 
     def load(self):
         if self.input_path.endswith('.txt'):
-            return self.load_txt()
+            self.load_txt()
         elif self.input_path.endswith('.csv'):
-            return self.load_csv()
+            self.load_csv()
         elif self.input_path.endswith('.xes'):
-            return self.load_xes()
+            self.load_xes()
         else:
             raise ValueError('[ERROR]: File extension not recognized')
 
